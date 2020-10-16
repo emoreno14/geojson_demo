424f0aa007088065a67325ee664000bb30845e5b
emoreno14 <72118376+emoreno14@users.noreply.github.com>
2020-10-16 17:23:02 -0500

    update geojson

--- geojson.js
+++ geojson.js
@@ -12,190 +12,210 @@ var json = {
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#e21212",
+        "marker-color": "#ff7b00",
         "marker-size": "medium",
         "marker-symbol": "",
-        "title": "example point",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50364904376/in/album-72157716054666967/\" title=\"Picture 7: Restricted Area: Chairs in Corner\"><img src=\"https://live.staticflickr.com/65535/50364904376_0d5200c551_n.jpg\" width=\"240\" height=\"320\" alt=\"Picture 7: Restricted Area: Chairs in Corner\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "date": "Sept. 2020"
+        "Title": "\"Please help us stay open, help us stay working.\"",
+        "Date": "September 22,2020",
+        "Description": "Image of a sign stating: wearing a mask is Nebraska nice. There is a red mask under the words.",
+        "Coverage": "Downtown Lincoln",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50494689953/in/dateposted-public/\" title=\"Image 1\"><img src=\"https://live.staticflickr.com/65535/50494689953_50b9505a03_m.jpg\" width=\"180\" height=\"240\" alt=\"Image 1\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70482516288757,
-          40.81622082257484
+          -96.70303344726562,
+          40.811178302949216
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#00ff1e",
+        "marker-color": "#00ccff",
         "marker-size": "medium",
         "marker-symbol": "",
-        "title": "love library",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50364909876/in/album-72157716054666967/\" title=\"Picture 6: Floor Arrow Stickers in the Library\"><img src=\"https://live.staticflickr.com/65535/50364909876_0b6cdbce90_n.jpg\" width=\"240\" height=\"320\" alt=\"Picture 6: Floor Arrow Stickers in the Library\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "description": "red sign stickers",
-        "month": "September",
-        "year ": 2020
+        "Title": "Together, We Beat This",
+        "Date": "September 22, 2020",
+        "Description": "image of sign stating: Together We Beat This. This is repeated three times. Under this is the COVID guidelines, which include: a mask, social distancing, and washing your hands. This sign is surrounded by other posters close to the soda station. ",
+        "Coverage": "The Sultan's Kite",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50495397006/in/dateposted-public/\" title=\"Image 2\"><img src=\"https://live.staticflickr.com/65535/50495397006_c06a731b0b_m.jpg\" width=\"180\" height=\"240\" alt=\"Image 2\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70298516750336,
-          40.8186079687254
+          -96.70208930969238,
+          40.813386989954935
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#dc06f9",
+        "marker-color": "#fbff00",
         "marker-size": "medium",
         "marker-symbol": "",
-        "title": "red covid sign",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50364237188/in/album-72157716054666967/\" title=\"Picture 1: &quot;Protect Yourself and Our Husker Community&quot; Sign\"><img src=\"https://live.staticflickr.com/65535/50364237188_1e1e5bb8b9_n.jpg\" width=\"320\" height=\"240\" alt=\"Picture 1: &quot;Protect Yourself and Our Husker Community&quot; Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "date": "Sept. 2020"
+        "Title": "Please Respect Social Distancing",
+        "Date": "September 22, 2020",
+        "Description": "image on Canopy Street Market's door window. The yellow sign states: Please respect social distancing throughout the store and at the register. Thanks.",
+        "Coverage": "Canopy Street Markey",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50494689383/in/dateposted-public/\" title=\"Image 3\"><img src=\"https://live.staticflickr.com/65535/50494689383_753414bc48_m.jpg\" width=\"180\" height=\"240\" alt=\"Image 3\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70368790626526,
-          40.817609275175144
+          -96.711745262146,
+          40.81293226628777
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#0a06f4",
+        "marker-color": "#706b6b",
         "marker-size": "medium",
         "marker-symbol": "",
-        "title": "\"Enter Only Sign\"",
-        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50364228558/in/album-72157716054666967/\" title=\"Picture 2: &quot;Enter Only&quot; Door Sign\"><img src=\"https://live.staticflickr.com/65535/50364228558_6d2af1d501_n.jpg\" width=\"240\" height=\"320\" alt=\"Picture 2: &quot;Enter Only&quot; Door Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+        "Title": "Please Wait Outside for Your Appointment",
+        "Date": "October 7, 2020",
+        "Description": "image of barber shop door and window with multiple signs. One of the sign states: Please wait outside for your appointment. Another sign states: Please call for an appointment. Another sign states: Please wear face covering at all times.",
+        "Coverage": "The Clipper Barber Shop",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50494689188/in/dateposted-public/\" title=\"Image 4\"><img src=\"https://live.staticflickr.com/65535/50494689188_11ea50dcf2_n.jpg\" width=\"240\" height=\"320\" alt=\"Image 4\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script> "
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70220732688904,
-          40.8193387105778
+          -96.70384883880615,
+          40.81286730550947
         ]
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#00fffb",
+        "marker-color": "#b505e6",
         "marker-size": "medium",
         "marker-symbol": "",
-        "Title": "Andrew's Hallway",
-        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50364915106/in/album-72157716054666967/\" title=\"Picture 4: Andrews Hall During COVID\"><img src=\"https://live.staticflickr.com/65535/50364915106_cb96590e35_n.jpg\" width=\"320\" height=\"240\" alt=\"Picture 4: Andrews Hall During COVID\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script> ",
-        "date": "Sept 2020"
+        "Title": "We've Adjusted Our Hours",
+        "Date": "October 7, 2020",
+        "Description": "image of Enterprise sign on a window. The sign informs the customers of the new business hours due to COVID. ",
+        "Coverage": "Enterprise",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50495553102/in/dateposted-public/\" title=\"Image 5\"><img src=\"https://live.staticflickr.com/65535/50495553102_886b34ed6f_n.jpg\" width=\"240\" height=\"320\" alt=\"Image 5\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70150995254517,
-          40.81937118780665
+          -96.70642375946045,
+          40.81122702477914
         ]
       }
     },
     {
       "type": "Feature",
-      "properties": {},
+      "properties": {
+        "marker-color": "#ff0000",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Title": "No Dining In",
+        "Date": "October 3, 2020",
+        "Description": "Image of red tape blocking brown tables. Panda Express.",
+        "Coverage": "Panda Express",
+        "Rights ": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50495551982/in/dateposted-public/\" title=\"Imagae 6\"><img src=\"https://live.staticflickr.com/65535/50495551982_fbba028075_m.jpg\" width=\"240\" height=\"127\" alt=\"Imagae 6\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
       "geometry": {
-        "type": "LineString",
+        "type": "Point",
         "coordinates": [
-          [
-            -96.7047929763794,
-            40.81637509652106
-          ],
-          [
-            -96.7047607898712,
-            40.81706526504568
-          ],
-          [
-            -96.70404195785522,
-            40.81708962380358
-          ],
-          [
-            -96.70405268669128,
-            40.817593036131385
-          ],
-          [
-            -96.70363426208495,
-            40.81768235082279
-          ]
+          -96.7017674446106,
+          40.8157092800809
         ]
       }
     },
     {
       "type": "Feature",
-      "properties": {},
+      "properties": {
+        "marker-color": "#1da701",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Title": "May Your Mask Be With You",
+        "Date": "October 7, 2020",
+        "Description": "Image of different ads on a board placed on the corner of 13th and O streets. All ads ecnourage people to wear their mask.",
+        "Coverage": "Downtown Lincoln",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50494686098/in/dateposted-public/\" title=\"Image 7\"><img src=\"https://live.staticflickr.com/65535/50494686098_c806d50887_m.jpg\" width=\"180\" height=\"240\" alt=\"Image 7\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
       "geometry": {
-        "type": "LineString",
+        "type": "Point",
         "coordinates": [
-          [
-            -96.7036771774292,
-            40.81782850187658
-          ],
-          [
-            -96.70405268669128,
-            40.81780414338999
-          ],
-          [
-            -96.7040205001831,
-            40.818291311422705
-          ],
-          [
-            -96.7030656337738,
-            40.818283191984804
-          ],
-          [
-            -96.70308709144592,
-            40.818664804491505
-          ],
-          [
-            -96.70297980308533,
-            40.8187054014375
-          ]
+          -96.70238971710205,
+          40.81341947009762
         ]
       }
     },
     {
       "type": "Feature",
-      "properties": {},
+      "properties": {
+        "marker-color": "#ff00c8",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Title": "No Mask No Service",
+        "Date": "October 7, 2020",
+        "Description": "Image of sign on Pearle Vision stating: No Mask No Service.",
+        "Coverage": "Pearle Vision",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50495393201/in/dateposted-public/\" title=\"Image 8\"><img src=\"https://live.staticflickr.com/65535/50495393201_780ba54a8e_m.jpg\" width=\"180\" height=\"240\" alt=\"Image 8\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
       "geometry": {
-        "type": "LineString",
+        "type": "Point",
         "coordinates": [
-          [
-            -96.70296907424925,
-            40.818693222356295
-          ],
-          [
-            -96.70306026935577,
-            40.818737878976386
-          ],
-          [
-            -96.7030656337738,
-            40.818843430868
-          ],
-          [
-            -96.7026150226593,
-            40.81928593504708
-          ],
-          [
-            -96.7026150226593,
-            40.81935088954049
-          ],
-          [
-            -96.70221269130707,
-            40.81936712815391
-          ],
-          [
-            -96.70149922370909,
-            40.81939148606658
-          ]
+          -96.70372009277344,
+          40.81341947009762
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#004cff",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Title": "Prevent The Spread of COVID and Don't Forget: No Human is Illegal",
+        "Date": "September 30, 2020",
+        "Description": "Image of three different signs about COVID.",
+        "Coverage": "Pan Dulce Bakery",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50495392866/in/dateposted-public/\" title=\"Image 9\"><img src=\"https://live.staticflickr.com/65535/50495392866_d3216b39eb_m.jpg\" width=\"180\" height=\"240\" alt=\"Image 9\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.705801486969,
+          40.80520148712018
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ffd500",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Title": "Increase the Spread of COVID",
+        "Date": "October 7, 2020",
+        "Description": "Image of a sign outside a strip club stating: Hot Girls, Cold Beers. ",
+        "Coverage": "Strip club on corner of M and 11th St.",
+        "Rights": "image property of Elva Moreno Del Rio, all rights reserved.",
+        "Image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190007611@N04/50495392551/in/dateposted-public/\" title=\"Image 10\"><img src=\"https://live.staticflickr.com/65535/50495392551_4bd546c9bc_m.jpg\" width=\"240\" height=\"180\" alt=\"Image 10\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70573711395264,
+          40.81122702477914
         ]
       }
     }
